43c8579139472ed4f193624cf77fbe3a592b509f
PTPocket <118230540+PTPocket@users.noreply.github.com>
2025-11-09 16:13:38 -0800

    first commit of complete terminal based program

---
+++ main.py
@@ -0,0 +1,99 @@
+import os
+from datetime import datetime
+import shutil
+#import time
+class Rule():
+    def __init__(self, destination_folder:str, file_types:list, identifiers:list):
+        self.destination_folder = destination_folder
+        self.file_types  = file_types
+        self.identifiers = identifiers
+
+    def file_exists(self, file_name:str):
+        path = os.path.join(self.destination_folder, file_name)
+        if os.path.exists(path) is True:
+            return True
+        return False
+    
+    def get_destination_path(self, file_name:str):
+        return os.path.join(self.destination_folder, file_name)
+    
+    def is_type_valid(self, file_name:str):
+        if self.file_types == []:
+            return True
+        for file_type in self.file_types:
+            if str(file_name).lower().endswith('.'+str(file_type).lower()):
+                return True
+        return False
+
+    def is_identifier_valid(self, file_name:str):
+        if self.identifiers == []:
+            return True
+        for identifier in self.identifiers:
+            if str(identifier).lower() in file_name.lower():
+                return True
+        return False
+
+    def is_valid(self, file_name:str):
+        if self.is_type_valid(file_name) is False or self.is_identifier_valid(file_name) is False:
+            return False
+        return True
+
+class TransferRule():
+    def __init__(self, name:str, source_folder:str, rules:list):
+        self.name   = name
+        self.source_folder = source_folder
+        self.rules  = rules
+
+    def print_log(self, copy_type:str, file_name:str, dest_path:str):
+        text = f'''
+____________________________
+Date   : {datetime.today().replace(microsecond=0)}
+Action : {copy_type.capitalize()}
+File   : {file_name}
+From   : {self.source_folder}
+To     : {dest_path}'''
+        print(text)
+    def run(self):
+        try:
+            for file in os.listdir(self.source_folder):
+                source_path = os.path.join(self.source_folder, file)
+                for rule in self.rules:
+                    if rule.is_valid(file) is False:
+                        continue
+                    dest_folder = rule.destination_folder
+                    dest_path = rule.get_destination_path(file)
+                    if rule.file_exists(file) is False:
+                        shutil.copy2(source_path, dest_path)
+                        self.print_log('Copy', file, dest_folder)
+                        continue
+                    #Checks if file in source has been modified
+                    if os.path.getmtime(source_path) > os.path.getmtime(dest_path):
+                        shutil.copy2(source_path, dest_path)
+                        self.print_log('Overwrite', file, dest_folder)
+                        continue
+        except Exception as e:
+            text=f'''
+____________________________
+!!!ERROR!!!
+Rule Name  : {self.name}
+Description: {e}'''
+            print(text)
+
+if __name__ == '__main__':
+    transfer_rules = [
+        TransferRule(
+            name = 'default name',
+            source_folder ='source',
+            rules = [
+                Rule(
+                    destination_folder='destination',
+                    file_types=['pdf'],
+                    identifiers=['letter']
+                )
+            ]
+        )
+    ]
+    print('File transfer started.')
+    while True:
+        for transfer_rule in transfer_rules:
+            transfer_rule.run()
